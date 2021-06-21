890eb6cf520b7e966c332285ce7ba3bf7be03436
Jarnotouw14 <Jarnotouw14@gmail.com>
2021-06-21 18:02:07 +0200

    update

--- utils/sockets.js
+++ utils/sockets.js
@@ -22,16 +22,15 @@ function startChatServer(io) {
             verifyMessage(message, message.username)
                 .then(() => {
                     const user = userJoin(socket.id, message.username, message.stream);
-
                     socket.join(user.stream);
 
-                    socket.emit('message', signMessage(formatMessage(botName, 'Welcome to the chat!', true)));
+                    socket.emit('message', signMessage(formatMessage(botName, 'Welcome to the chat!', user.stream, true)));
 
                     socket.broadcast
                     .to(user.stream)
                     .emit(
                         'message',
-                        signMessage(formatMessage(botName, `${user.username} has joined the chat`, true))
+                        signMessage(formatMessage(botName, `${user.username} has joined the chat`, user.stream, true))
                     );
 
                     io.to(user.stream).emit('streamUsers', signMessage(getStreamUsers(user.stream)));
@@ -42,11 +41,11 @@ function startChatServer(io) {
         });
 
         socket.on('chatMessage', msg => {
-            verifyMessage(msg)
+            verifyMessage(msg, msg.username)
                 .then(() => {
                     const user = getCurrentUser(socket.id);
-                    emitMessage(user, formatMessage(user.username, msg.message, false));
-                    SaveMongoDB(message, user, msg.signature, verified);
+                    emitMessage(user, formatMessage(user.username, msg.message, user.stream, false));
+                    SaveMongoDB(msg.message, user, msg.signature, true);
                 });
         });
 
@@ -54,18 +53,18 @@ function startChatServer(io) {
             const user = userLeave(socket.id);
 
             if (user) {
-                emitMessage(user, formatMessage(botName, `${user.username} has left the chat`, true));
+                emitMessage(user, formatMessage(botName, `${user.username} has left the chat`, user.stream, true));
                 io.to(user.stream).emit('streamUsers', signMessage(getStreamUsers(user.stream)));
             }
         });
 
         socket.on('disconnectUserFromStream', msg => {
-            verifyMessage(msg)
+            verifyMessage(msg, msg.username)
                 .then(() => {
                     const user = userLeave(msg.message);
 
                     if (user) {
-                        emitMessage(user, formatMessage(botName, `${user.username} has left the chat`, true));
+                        emitMessage(user, formatMessage(botName, `${user.username} has left the chat`, user.stream, true));
                         io.to(user.stream).emit('streamUsers', signMessage(getStreamUsers(user.stream)));
                     }
                 });
@@ -75,25 +74,26 @@ function startChatServer(io) {
     function emitMessage(user, message){
         io.to(user.stream).emit('message', signMessage(message));
     }
-  }
 
-function SaveMongoDB(message, user, signature, verified)
-{
-    MongoClient.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
-        if (err) throw err;
+    function SaveMongoDB(message, user, signature, verified){
+        MongoClient.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+            if (err) throw err;
 
-        const db = client.db("chat_room");
-        let document = {_id: new ObjectID(), message: message.text, user_id: user.id, verified: verified, time: message.timeWithMilliSeconds, stream: user.stream, signature: signature };
+                const db = client.db("chat_room");
+                let document = {_id: new ObjectID(), message: message.text, user_id: user.id, verified: verified, time: message.timeWithMilliSeconds, stream: user.stream, signature: signature };
 
-        db.collection('chat_history').insertOne(document).then((saveObject) => {
-            console.log('Message inserted')
-            console.log(saveObject);
-        }).catch((err) => {
-            console.log(err);
-        }).finally(() => {
-            //client.close();
+                db.collection('chat_history').insertOne(document).then((saveObject) => {
+                console.log('Message inserted')
+                console.log(saveObject);
+            }).catch((err) => {
+                console.log(err);
+            }).finally(() => {
+                //client.close();
+            });
         });
-    });
+    }
 }
 
-  module.exports = startChatServer;
+
+
+module.exports = startChatServer;
