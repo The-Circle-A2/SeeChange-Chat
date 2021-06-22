96a02333ea14cce468690eb0b09b895cab33a40a
Bart Fijneman <info@bartfijneman.com>
2021-06-23 01:28:27 +0200

    Added cors

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ const express = require('express');
 const socketio = require('socket.io');
 const startChatServer = require('./utils/sockets');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const app = express();
+app.use(cors());
+
 const server = http.createServer(app);
-const io = socketio(server, {rejectUnauthorized: false});
+const io = socketio(server, {
+    allowEIO3: true
+});
+
 dotenv.config();
 io.set('origins', process.env.CLIENT_URL);
 
