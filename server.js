00e3ec1835e636fba9e2e9b26d5033d7f8340d37
Bart Fijneman <info@bartfijneman.com>
2021-06-23 01:06:12 +0200

    Correct jsencrypt import

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const startChatServer = require('./utils/sockets');
 const dotenv = require('dotenv');
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {rejectUnauthorized: false});
 dotenv.config();
 io.set('origins', process.env.CLIENT_URL);
 
